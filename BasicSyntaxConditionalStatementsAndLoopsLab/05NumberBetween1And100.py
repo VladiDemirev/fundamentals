02577f0e5381ffe7c7d54b5b8a703b60c147c0ba
Vladimir Demirev <v.demirev@yahoo.com>
2023-04-29 18:05:50 +0300

    Write a program that reads different floating-point numbers and performs action if the number is between 1 and 100

---
+++ BasicSyntaxConditionalStatementsAndLoopsLab/05NumberBetween1And100.py
@@ -0,0 +1,10 @@
+while True:
+    user_input = float(input())
+    if 1 <= user_input <= 100:
+        print(f"The number {user_input} is between 1 and 100")
+        break
+
+# while user_input < 1 or user_input > 100:
+#     user_input = float(input())
+#
+# print(f"The number {user_input} is between 1 and 100")
