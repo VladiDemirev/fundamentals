20feabbfede7624755767cca91db02985b73d66d
Vladimir Demirev <v.demirev@yahoo.com>
2023-06-20 18:52:32 +0300

    Write a program that reads four integer numbers. It should add the first to the second number, integer divide the sum by the third number, and multiply the result by the fourth number. Print the final result.

--- DataTypesAndVariablesExercise/01IntegerOperations.py
+++ DataTypesAndVariablesExercise/01IntegerOperations.py
@@ -1,8 +1,20 @@
+# int1 = int(input())
+# int2 = int(input())
+# int3 = int(input())
+# int4 = int(input())
+#
+# sum_int = (int((int1 + int2) / int3) * int4)
+#
+# print(sum_int)
+
+
+# OPTION 2
+
 int1 = int(input())
 int2 = int(input())
 int3 = int(input())
 int4 = int(input())
 
-sum_int = (int((int1 + int2) / int3) * int4)
+sum_int = ((int1 + int2) // int3) * int4
 
 print(sum_int)
