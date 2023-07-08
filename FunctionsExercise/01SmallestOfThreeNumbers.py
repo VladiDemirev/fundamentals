603a7d60f5b775e577194f7b1535933f6cf3a13a
Vladimir Demirev <v.demirev@yahoo.com>
2023-07-08 19:34:20 +0300

    Write a function that receives three integer numbers and returns the smallest. Print the result on the console. Use an appropriate name for the function.

---
+++ FunctionsExercise/01SmallestOfThreeNumbers.py
@@ -0,0 +1,9 @@
+def smallest_number(int1, int2, int3):
+    return min(int1, int2, int3)
+
+
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+print(smallest_number(num1, num2, num3))
