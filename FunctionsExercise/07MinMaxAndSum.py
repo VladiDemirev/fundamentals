54f15c2f9a167ca09ce9080176996ce2453a20a0
Vladimir Demirev <v.demirev@yahoo.com>
2023-07-15 14:46:50 +0300

    Write a program that receives a sequence of numbers (integers) separated by a single space. It should print the min and max values of the given numbers and the sum of all the numbers in the list. Use min(), max() and sum().

---
+++ FunctionsExercise/07MinMaxAndSum.py
@@ -0,0 +1,26 @@
+int_sequence = [int(x) for x in input().split()]
+
+print(f"The minimum number is {min(int_sequence)}")
+print(f"The maximum number is {max(int_sequence)}")
+print(f"The sum number is: {sum(int_sequence)}")
+
+
+#   OPTION 2
+
+# def minimum(list_numbers):
+#   min_number = min(list_numbers)
+#   return min_number
+#
+# def maximum(list_numbers):
+#   max_number = max(list_numbers)
+#   return max_number
+#
+# def sum_all(list_numbers):
+#   sum_all_numbers = sum(list_numbers)
+#   return sum_all_numbers
+#
+# int_sequence = [int(x) for x in input().split()]
+#
+# print(f"The minimum number is {minimum(int_sequence)}")
+# print(f"The maximum number is {maximum(int_sequence)}")
+# print(f"The sum number is: {sum_all(int_sequence)}")
